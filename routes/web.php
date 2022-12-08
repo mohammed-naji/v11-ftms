d7bc21f771fd88d5c2e3ddabbcb95b41514e1a07
Mohammed Naji <m.naji1512@gmail.com>
2022-12-08 18:04:57 +0200

    auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
-use App\Http\Controllers\RelationController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RelationController;
+use App\Http\Controllers\Admin\AdminController;
 
-Route::get('profile', [RelationController::class, 'profile']);
+// Route::get('profile', [RelationController::class, 'profile']);
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+});
+
+Route::view('/', 'welcome');
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
