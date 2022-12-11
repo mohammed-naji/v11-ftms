749979d77cbdb84df5ac29ace14e741603d95a2a
Mohammed Naji <m.naji1512@gmail.com>
2022-12-11 18:03:31 +0200

    lec22

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function index($lang = 'en')
     {
+        // if(in_array($lang, ['ar', 'en', 'fr'])) {
+        //     App::setLocale($lang);
+        // }
+        // else {
+        //     dd('اختار لغة صح ي حبيبي بكفي هبل !!!!!!!');
+        // }
+
         return view('admin.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,20 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RelationController;
 use App\Http\Controllers\Admin\AdminController;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 // Route::get('profile', [RelationController::class, 'profile']);
 
-Route::prefix('admin')->name('admin.')->group(function() {
-    Route::get('/', [AdminController::class, 'index'])->name('index');
+// Route::group(['prefix' => LaravelLocalization::setLocale()], function() {
+Route::prefix(LaravelLocalization::setLocale())->group(function() {
+    Route::prefix('admin')->name('admin.')->group(function() {
+        Route::get('/', [AdminController::class, 'index'])->name('index');
+    });
+
+    Route::view('/', 'welcome');
+
+    Auth::routes(['verify' => true]);
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
+
 });
-
-Route::view('/', 'welcome');
-
-Auth::routes(['verify' => true]);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
